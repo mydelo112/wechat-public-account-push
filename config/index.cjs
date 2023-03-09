1bc72232872fcbda17c4698cf93f133bfa29fe96
mydelo112 <127375146+mydelo112@users.noreply.github.com>
2023-03-09 13:25:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,10 +33,10 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '婧', year: '2000', date: '12-15',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2023', date: '2-24',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
